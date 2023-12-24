8dccde15f08c4a09fb896280e1db5e1c8a6aa197
Apozn <apoznaharev@gmail.com>
2023-12-24 16:36:54 +0300

    Add save_photos_from_zip

--- save.py
+++ save.py
@@ -7,6 +7,8 @@ from telegram.ext import CallbackContext
 import constants
 from operation import load_photo_list
 from validator import stop_if_not_oracle
+import zipfile
+from io import BytesIO
 
 
 def save_foto(update: Update, context: CallbackContext) -> None:
@@ -65,4 +67,42 @@ def delete_photo_by_command(update: Update, context: CallbackContext):
     except Exception as e:
         update.message.reply_text(
             f"Произошла ошибка при удалении фотографии: {str(e)}"
+        )
+
+
+def save_photos_from_zip(update: Update, context: CallbackContext) -> None:
+    """Метод сохранения фотографий из zip-архива."""
+    message = update.message
+    chat_id = message.chat_id
+    if stop_if_not_oracle(chat_id, context):
+        return
+    if message.document and message.document.mime_type == 'application/zip':
+        file_id = message.document.file_id
+        file = context.bot.get_file(file_id)
+        zip_file = BytesIO(file.download_as_bytearray())
+        
+        # Путь к папке, куда будут сохранены фотографии
+        save_folder = "mems_actual"
+        
+        with zipfile.ZipFile(zip_file) as z:
+            for file_info in z.infolist():
+                with z.open(file_info.filename) as photo_file:
+                    file_extension = os.path.splitext(file_info.filename)[-1]
+                    file_name = (
+                        f"mem_{message.date.strftime('%Y.%m.%d')}_{message.message_id}"
+                        f"{file_extension}"
+                    )
+                    file_path = os.path.join(save_folder, file_name)
+                    with open(file_path, 'wb') as f:
+                        f.write(photo_file.read())
+        
+        load_photo_list()
+        context.bot.send_message(
+            chat_id=chat_id,
+            text="Фотографии успешно сохранены из zip-архива!"
+        )
+    else:
+        context.bot.send_message(
+            chat_id=chat_id,
+            text="Пожалуйста, отправьте zip-архив с фотографиями."
         )
